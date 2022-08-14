96c3f1968668415b5200f19e67f92f935ddc2101
Moises2005-og <78978227+Moises2005-og@users.noreply.github.com>
2022-08-14 16:39:24 +0100

    Initial commit

---
+++ Aula 11/ex008.js
@@ -0,0 +1,6 @@
+let carro = 12
+console.log(`Você está dirigindo a ${carro}km/h`)
+if(carro >= 60) {
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+console.log(`Dirija sempre com cuidado`)
---
+++ Aula 11/ex009.js
@@ -0,0 +1,7 @@
+let country = 'Angola'
+console.log(`Vivendo em ${country}`)
+if(country == 'Angola') {
+    console.log('Você é angolano')
+} else {
+    console.log('Você é estrangeiro')
+}
